64464cb7411c1cee3af6a30e6d664c7486118d83
Ph0tonz <nwmechanic87@gmail.com>
2023-01-14 12:26:10 +0000

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-#create databases
-CREATE DATABASE IF NOT EXISTS `wordpress`;
-CREATE DATABASE IF NOT EXISTS `dingo`;
-CREATE DATABASE IF NOT EXISTS `blog`;
-
-#create user
-CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';
-FLUSH PRIVILEGES;
